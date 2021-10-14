0c17179fe9da67792faf001f217a2fb12a24d201
waldoeGeek <thomas.reynolds@student.sl.on.ca>
2021-10-14 14:00:21 -0400

    init

---
+++ js/script.js
@@ -0,0 +1,35 @@
+`use strict`;
+
+const messageElement = document.querySelectorAll('.user-nav__icon-box')[1];
+const userElement = document.querySelector('.user-nav__user');
+const userPop = document.querySelector('.user-nav__popup');
+const chatBox = document.querySelector('.user-nav__chat-box');
+
+let chatIsOpen = false;
+let popIsOpen = false;
+
+console.log(messageElement);
+
+messageElement.addEventListener('touchstart', () => {
+  if (popIsOpen) {
+    userPop.classList.remove('open');
+    chatBox.classList.toggle('open');
+    popIsOpen = false;
+    chatIsOpen = true;
+  } else {
+    chatBox.classList.toggle('open');
+    chatIsOpen = true;
+  }
+});
+
+userElement.addEventListener('touchstart', () => {
+  if (chatIsOpen) {
+    chatBox.classList.remove('open');
+    userPop.classList.toggle('open');
+    chatIsOpen = false;
+    popIsOpen = true;
+  } else {
+    userPop.classList.toggle('open');
+    popIsOpen = true;
+  }
+});
